d6c905ddffcffdc8f042bf90de141a8b2e13419e
YouRim Kim <20203010@yiu.ac.kr>
2021-05-12 19:48:26 +0900

    jdbtestt

---
+++ jdbctestt/src/jdbctestt/DBConnect02.java
@@ -0,0 +1,56 @@
+package jdbctestt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBConnect02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			String url = "jdbc:mysql://localhost/world?characterEncoding=UTF-8&serverTimezone=UTC";
+			String user = "root";
+			String passwd = "root11";
+			Connection con = null;
+			String sql = "select * from oneline";
+			PreparedStatement pstmt = null;
+			PreparedStatement pstmt2 = null;
+		
+			Class.forName("com.mysql.jdbc.Driver");
+		
+			con = DriverManager.getConnection(url, user, passwd);
+			
+			pstmt2 = con.prepareStatement("use world");
+			pstmt2.executeUpdate();
+			pstmt2.close();
+			
+			pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				int no = rs.getInt(1);
+				String memo = rs.getString("memo");
+				String wdate = rs.getString(3);
+				
+				System.out.println(no +":"+memo+ ":("+wdate+")");
+			}
+			rs.close();
+			pstmt.close();
+			//pstmt.executeUpdate();		
+			con.close();
+			System.out.println("DB연결 성공");
+		}
+		catch (SQLException e)
+		{
+			System.out.println("DB연결 실패");
+			System.out.print("사유 : " + e.getMessage());
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}		
+	}
+}
